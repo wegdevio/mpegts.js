d583ab3dfa97cfda8bb73f12cf450976338874ac
awdn <awdn@nolook.pass>
2026-01-19 13:38:30 +0800

    chore: add event key

--- d.ts/src/core/mse-events.d.ts
+++ d.ts/src/core/mse-events.d.ts
@@ -4,6 +4,8 @@ declare enum MSEEvents {
     UPDATE_END = "update_end",
     BUFFER_FULL = "buffer_full",
     START_STREAMING = "start_streaming",
-    END_STREAMING = "end_streaming"
+    END_STREAMING = "end_streaming",
+    SOURCE_ENDED = "source_ended",
+    SOURCE_CLOSE = "source_close"
 }
 export default MSEEvents;
